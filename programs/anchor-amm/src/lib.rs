aeaff27e037a205e6d16c5aac3c9bcea41e95076
Adetunji Ojekunle <ojekunleadetunji@gmail.com>
2026-05-25 20:32:32 +0100

    Add instruction functions to lib file

--- programs/anchor-amm/src/lib.rs
+++ programs/anchor-amm/src/lib.rs
@@ -24,9 +24,15 @@ pub mod anchor_amm {
         ctx.accounts.init(seed, fee, authority, ctx.bumps)
     }
 
-    // pub fn deposit(
-    //     ctx: Context<Deposit>
-    // ){}
-    // pub fn withdraw(){}
-    // pub fn swap(){}
+    pub fn deposit(ctx: Context<Deposit>, amount: u64, max_x: u64, max_y: u64) -> Result<()> {
+        ctx.accounts.deposit(amount, max_x, max_y)
+    }
+
+    pub fn withdraw(ctx: Context<Withdraw>, amount: u64, min_x: u64, min_y: u64) -> Result<()> {
+        ctx.accounts.withdraw(amount, min_x, min_y)
+    }
+
+    pub fn swap(ctx: Context<Swap>, is_x: bool, amount_in: u64, min_amount_out: u64) -> Result<()> {
+        ctx.accounts.swap(is_x, amount_in, min_amount_out)
+    }
 }
